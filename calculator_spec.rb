d09ee56f08c9af0e690c501d237bc7ee29bf409f
kohrVid <kohrVid@zoho.com>
2016-05-23 12:07:56 +0100

    Committing tests

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -5,12 +5,30 @@ class CalculatorSpec < MiniTest::Test
   def test_accumulator
     my_calculator = Calculator.new()
     my_calculator.accumulator = 5
-    assert_equal(my_calculator.accumulator, 5)
+    assert_equal(5, my_calculator.accumulator)
   end
 
   def test_add
     my_calculator = Calculator.new
     my_calculator.accumulator = 5
-    assert_equal(my_calculator.add(7), 12)
+    assert_equal(12, my_calculator.add(7))
+  end
+  
+  def test_minus
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(-2, my_calculator.minus(7))
+  end
+
+  def test_multiply
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(35, my_calculator.multiply(7))
+  end
+  
+  def test_division
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(0.7142857142857143, my_calculator.divide(7))
   end
 end
