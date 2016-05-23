b9a17e3fb0df0a9c4d8f730b7443867ec2fe964c
kohrVid <kohrVid@zoho.com>
2016-05-23 11:43:37 +0100

    Accumulator implementation

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -2,10 +2,9 @@ require "minitest/autorun"
 require "./calculator"
 
 class CalculatorSpec < MiniTest::Test
-  def setup
+  def test_accumulator
     my_calculator = Calculator.new()
     my_calculator.accumulator = 5
     assert_equal(my_calculator.accumulator, 5)
   end
-
 end
